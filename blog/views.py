6d1d29f938671e22b715ab62b2cc59db652cd4d4
osya <taira@mbd.nifty.com>
2019-07-30 17:42:32 +0900

    modfied view.py.

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ def post_list(request):
 #    return render(request, 'blog/post_list.html', {})
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, pk): 
+def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
     
